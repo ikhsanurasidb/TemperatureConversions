d7e245092282cf5fae269e2153696199331162d0
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-06-27 07:52:32 +0700

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Muhamamd Nurasid on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
